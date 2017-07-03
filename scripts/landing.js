45f5901ba66da6ebf44641f62db135127eb8853e
Sam Smith <smithrunner@live.com>
2017-07-03 09:19:14 -0500

    Checkpoint 9 Complete

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,16 +1,29 @@
-var animatePoints = function() {
+var pointsArray = document.getElementsByClassName('point');
 
-    var points = document.getElementsByClassName('point');
+var animatePoints = function(points) {
+    var revealPoint = function(index) {
+          points[index].style.opacity = 1;
+          points[index].style.transform = "scaleX(1) translateY(0)";
+          points[index].style.msTransform = "scaleX(1) translateY(0)";
+          points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
+    }
 
-    var revealPoint = function() {
-        for (var i=0; i<3; i++) {
-          points[i].style.opacity = 1;
-          points[i].style.transform = "scaleX(1) translateY(0)";
-          points[i].style.msTransform = "scaleX(1) translateY(0)";
-          points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
-        }
-    };
-
-    revealPoint();
-
+    for (var i = 0; i < points.length; i++) {
+          revealPoint(i);
+    }
 };
+
+window.onload = function() {
+    if (window.innerHeight > 950) {
+        animatePoints(pointsArray);
+    }
+    
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+
+    window.addEventListener('scroll', function(event) {
+        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+            animatePoints(pointsArray);
+        }
+    });
+}
